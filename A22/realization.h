35e80f4b5b5de31bbca02a24911ce64253b84122
RyogoTabuchi <ryogo.1008@gmail.com>
2021-06-11 10:14:09 +0900

    Add insert()

--- A22/realization.h
+++ A22/realization.h
@@ -31,7 +31,7 @@ public:
   const_iterator begin() const { return val.begin(); }
   const_iterator end() const { return val.end(); }
 
-  iterator insert(const value_type::value_type&);
+  std::pair<iterator, bool> insert(value_type::value_type);
 private:
   value_type val;
 };
