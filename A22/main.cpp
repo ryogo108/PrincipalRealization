0922d81f25c0abd8369f635e04bc15df2e9c45e6
RyogoTabuchi <ryogo.1008@gmail.com>
2021-06-11 11:20:33 +0900

    Add two constatnts alpha1 and alpha2

--- A22/main.cpp
+++ A22/main.cpp
@@ -5,8 +5,23 @@ using std::cout;  using std::endl;
 
 int main()
 {
+  const F w("PRIM_ROOT_OF_UNITY");
+  cout << CoxeterNum << "th RootOfUnity w = " << w << endl;
+  cout << "CF<" << CoxeterNum
+       << ">::MOD = " << CF<CoxeterNum>::MOD << endl;
+  const H alpha1 = {w / (w - (w^ -1)), -(w^ -1) / (w - (w^ -1))};
+  const H alpha2 = {F(-1) / (w - (w^ -1)), F(1) / (w - (w^ -1))};
+
   H a;
   S v;
+  v.insert(make_pair(monomial({std::make_pair(a, -1)}), F(1)));
+  cout << "w / (w - (w^-1)) = " << w / (w - (w^-1)) << endl;
+  cout << "A2 simple roots are followings:"
+       << endl
+       << "alpha1 = " << alpha1
+       << endl
+       << "alpha2 = " << alpha2
+       << endl;
   cout << "a = " << a << endl;
   cout << "v = " << v << endl;
   cout << "X(a, -3) * v = " << X(a, -3) * v << endl;
