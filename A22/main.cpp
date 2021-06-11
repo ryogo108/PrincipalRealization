8698d02836ed4f2533361bb6ee7ed0a5050688ce
RyogoTabuchi <ryogo.1008@gmail.com>
2021-06-11 13:06:19 +0900

    Use alpha1

--- A22/main.cpp
+++ A22/main.cpp
@@ -9,19 +9,21 @@ int main()
   cout << CoxeterNum << "th RootOfUnity w = " << w << endl;
   cout << "CF<" << CoxeterNum
        << ">::MOD = " << CF<CoxeterNum>::MOD << endl;
-  const H alpha1 = {w / (w - (w^ -1)), -(w^ -1) / (w - (w^ -1))};
-  const H alpha2 = {F(-1) / (w - (w^ -1)), F(1) / (w - (w^ -1))};
-
-  H a;
-  S v;
-  v.insert(make_pair(monomial({std::make_pair(a, -1)}), F(1)));
+  const H alpha1 = {(F(2) - w) / F(3),
+                    (F(1) + w) / F(3)};
+  const H alpha2 = {(F(-1) + F(2) * w) / F(3),
+                    (F(1) - F(-2) * w) / F(3)};
   cout << "A2 simple roots are followings:"
        << endl
        << "alpha1 = " << alpha1
        << endl
        << "alpha2 = " << alpha2
        << endl;
-  cout << "a = " << a << endl;
+
+  H a = alpha1;
+  S v;
+  v.insert(make_pair(monomial({std::make_pair(a, -1)}), F(1)));
+  cout << "a = alpha1 = " << a << endl;
   cout << "v = " << v << endl;
   cout << "X(a, -3) * v = " << X(a, -3) * v << endl;
   return 0;
