df939cc294a46ec6beaa3c4acc6f08837327d483
RyogoTabuchi <ryogo.1008@gmail.com>
2021-06-15 21:26:33 +0900

    fix E_minus

--- A22/realization.cpp
+++ A22/realization.cpp
@@ -436,7 +436,7 @@ Actions E_minus(const H& a, int n)
   Operators val;
   Operators A;
   for(size_t i = Operators::DEG0 + 1; i < Operators::MAX_DEG; ++i) {
-    A[-i + Operators::DEG0] = (F(CoxeterNum) / F(i)) * Actions(Factor(a, i));
+    A[-i + Operators::DEG0] = (F(CoxeterNum) / F(i - Operators::DEG0)) * Actions(Factor(a, i));
   }
   for(int i = 0; i < (Operators::MAX_DEG - Operators::DEG0); ++i) {
     val += (F(1) / factorial(i)) * pow(A, i);
